1fad1c07c654532e6fdd506bcbf013cbebc994cd
Moises Pahati <moisespahati@gmail.com>
2023-07-18 10:06:48 +0800

    update className and index.css

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
 function Accordion({data}) {
   return (
     <div className="accordion">
-      {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} />)}
+      {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />)}
     </div>
   );
 }
@@ -46,7 +46,7 @@ function AccordionItem({ num, title, text }) {
     <div className={`item ${isOpen ? 'open' : ''}`}
         onClick={handleToggle} >
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="text">{title}</p>
+      <p className="title">{title}</p>
       <p className="icon"
         style={{
           fontSize: '24px',
